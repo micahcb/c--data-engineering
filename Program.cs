6b757a143f12ebf99f35bf8fe8f0578aaf476eb3
micahcb <micahcb@umich.edu>
2023-01-16 08:35:38 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+
+// See https://aka.ms/new-console-template for more information
+
+namespace HelloWorld
+{
+
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int [] arr = {1, 2, 3, 4, 5};
+            Functions_class.add(arr);
+        }
+    }
+    public class Functions_class
+    {
+        public static void add(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
